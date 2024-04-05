22cadbb4e0ff21182007e89a3c6d3ed09fbe5d26
muna <muna3828@gmail.com>
2024-04-05 23:20:06 +0600

    created login and register page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <div>
+            login now
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,10 @@
+
+const Register = () => {
+    return (
+        <div>
+            register now
+        </div>
+    );
+};
+
+export default Register;
